fc626a5605aff8058342420fcfb0b176868b9854
sugar-syrup <120775682+sugar-syrup@users.noreply.github.com>
2023-01-15 13:30:46 +0530

    Fixed Q3

--- Lab_Exercise_1.py
+++ Lab_Exercise_1.py
@@ -76,36 +76,41 @@ def insertion_sort(list1):
     list1[j+1] = current
   return list1
 
-
-def my_sort(list1,k):
-  for i in range(1,len(list1)):
-    # Sorting the pos k(3 in example) elements in ascending order
-    if (i+1)%k == 0:
-      current = list1[i]
-      j = i -k
-      while j >= 0 and current < list1[j]:
-        list1[j+k] = list1[j]
-        j = j - k
-      list1[j+k] = current
+def mySort(List,k):
+    #Sorting in Ascending order
+    for i in range(k-1,len(List),k):
+        current = List[i]
+        j = i-k
+        while j>=0 and List[j] > current:
+            List[j+k] = List[j]
+            j-=k
+        List[j+k]=current
+        
+    #Sorting in Descending order
+    for i in range(len(List)):
+        if (i+1)%k!=0:
+            current = List[i]
+            if i%k == 0:
+                j = i-2
+            else:
+                j = i-1
+            while j>=0 and List[j]<current:
+                if (j+2)%k==0:
+                    List[j+2]=List[j]
+                else:
+                    List[j+1]=List[j]
+                if j%k == 0:
+                    j-=2
+                else:
+                    j-=1
+            if (j+2)%k == 0:
+                List[j+2]=current
+            else:
+                List[j+1]=current
     
-
-    # Sorting the rest of the elements in descending order
-    else:
-      current = list1[i]
-      j = i - 1
-      while j >= 0 and current > list1[j]:
-        if (j+1)%k == 0:
-          j = j - 1
-          list1[j+2] = list1[j]
-        else:
-          list1[j+1] = list1[j]
-        j = j -1 
-      list1[j+1] = current
-  return list1
-
-n = [31,12,21,55,14,1,51,30,2,7]
-k = 3
-print(my_sort(n,k))
+    return List
+print(mySort([31,12,21,55,22,1,51,30,2,7],3))
+print(mySort([10,7,9,8],3)) #Rosha, here you go
 print()
 
 
