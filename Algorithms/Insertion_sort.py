75fd15ae98f3f123a4b16138c77634f29b7cd27c
Harisankar <94229107+Methaphur@users.noreply.github.com>
2023-01-22 16:35:27 +0530

    Added Recursive Insertion_sort

--- Algorithms/Insertion_sort.py
+++ Algorithms/Insertion_sort.py
@@ -11,3 +11,20 @@ def insertion_sort(arr):
 
 
 # Insertion Sort Algorithm using Recursion
+def recursive_sort(arr):
+    if (len(arr)<=1):
+        return arr
+    else:
+        a1=recursive_sort(arr[:len(arr)-1])
+        current=arr[len(arr)-1]
+        a1.append(current)
+        j=len(a1)-2
+        while (j>=0) and (current<a1[j]):
+            a1[j+1]=a1[j]
+            j=j-1
+        a1[j+1]=current
+        return a1
+
+n = [1,6,3,21,17,19,34]
+print(insertion_sort(n))
+print(recursive_sort(n))
