7a444214391325438e5789d73e7610ad2388b70a
Harisankar <94229107+Methaphur@users.noreply.github.com>
2023-02-10 10:20:00 +0530

    Quiz 2.b Added

--- Quiz_1(03_02_2023).py
+++ Quiz_1(03_02_2023).py
@@ -70,5 +70,43 @@ print("Qs 2.a")
 print(my_sort(list1,N))
 print()
 
+# Question 2.b)
+# Jump one stair at a time or i steps when on ith step
+# f(n) = f(n-1)           n is odd
+# f(n) = f(n-1) + f(n/2)  n is even
+
+def count_way(n):
+  if n <= 2:  # Init base condition for step 1 and 2 
+    return 1
+
+  if n % 2 != 0:  
+    return count_way(n-1) 
+  else:
+    return count_way(n-1) + count_way(n/2)
+n = 8
+print("Qs 2.b")
+print(count_way(n))
+
+# To complete the function in O(n) time , we are expected to use Memorization (DP)
+def opt_count_way(n):
+  count = [0]*n  # Init a memory list with n zeroes
+  count[0] , count [1] = 1,1   # Setting base case for step 1 and 2 as 1
+  def count_ways(n,count):
+    # f(n) = f(n-1)  n is odd
+    if n % 2 != 0:
+      if count[n-1] == 0: # nth element is not calculated
+        count[n-1] = count_ways(n-1,count)
+      return count[n-1]
+
+    # f(n) = f(n-1) + f(n/2)
+    else:
+      if count[n-1] == 0: 
+        count[n-1] = count_ways(n-1,count) + count_ways(n//2,count)
+      return count[n-1]
+  return count_ways(n,count)
+print(opt_count_way(n))
+
+
+
 # Question 3 : just search for kth element of 2 sorted lists on geeks for geeks
 # Babunga code too lazy to comment/type
