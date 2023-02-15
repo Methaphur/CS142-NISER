cb18b5463c08d84981dd00148fff68b7a2006d5b
Harisankar <94229107+Methaphur@users.noreply.github.com>
2023-02-15 17:21:26 +0530

    Added qs 1 and 3

--- Assignments/Assignment_5(13_02_2023).py
+++ Assignments/Assignment_5(13_02_2023).py
@@ -1,4 +1,98 @@
+#Problem 1:
+'''Given a set of non-negative integers, a value t, and an integer k, determine
+if there is a subset of size exactly k such that the sum of the elements in the set is equal to
+t. Use backtracking to reduce the number of calls you make to your recursive function.'''
+
+def subset_sum(arr, target, length):
+    arr.sort() # Sorting the array so that weight gradually increases
+    def is_subset_sum(current, weight, current_subset):
+        # If sum has gone past the target element : skip
+        if weight + arr[current] > target: 
+            return False  
+        
+        # If the sum till current element is target 
+        elif weight + arr[current] == target:
+            # If the length of the subset is required length
+            if len(current_subset) + 1 == length:   
+                return True
+            else:
+                return False
+
+        # Current has gone through all the elements in the list
+        # Still no such sum pair has been found 
+        if current == len(arr) - 1:
+            return False
+        
+        else:
+        # We call the subset function twice , once where we include the current element in the sum
+        # And one where we exclude the current element in the sum
+          include = is_subset_sum(current+1, weight + arr[current], current_subset + [arr[current]]) 
+          exclude = is_subset_sum(current+1, weight, current_subset)
+
+          return  include or exclude
+    
+    return is_subset_sum(0, 0, [])
+
+
+input = [3,34,4,5,12,5,2]
+target = 9
+length = 2
+print(subset_sum(input,target,length))
+
+# Problem 1.b)
+'''Given a set of positive and negative integers, and a value t, and an integer k determine
+if there is a subset of size exactly k, such that the sum of the elements in the set is equal to
+t. Write a program that uses the function in Problem 1 as a subroutine.'''
+
+# We add a a positive number to all the list entries and update the target element correspondingly 
+# Such a positive number is chosen so that all the negative elements in the list beome positive
+
+def mod(x): # Completely redundant just use in-built abs() funtion
+  if x < 0:
+    return -x
+  else:
+    return x
+
+def subset_sum(arr, target, length): 
+    arr.sort()
+    a = mod(min(arr))   # Finding mod of min list entry
+    arr = [i+a for i in arr] # Adding that |min| to every element in list
+    target = target + length*a  # Updating target to target + Length*|min|
+
+    def is_subset_sum(current, weight, current_subset):
+        if weight + arr[current] > target:
+            return False  
+        
+        elif weight + arr[current] == target:
+            if len(current_subset) + 1 == length:
+                return True
+            else:
+                return False
+
+        if current == len(arr) - 1:
+            return False
+        
+        else:
+          include = is_subset_sum(current+1, weight + arr[current], current_subset + [arr[current]]) 
+          exclude = is_subset_sum(current+1, weight, current_subset)
+          return  include or exclude
+    
+    return is_subset_sum(0, 0, [])
+
+
+input = [3,34,4,5,12,5,2,-3,-5,-4]
+target = -12
+length = 3
+print(subset_sum(input,target,length))
+
+# Problem 2
 # N Queen problem
+'''Given an n×n chess board, you must place n queens on the board so that no
+two queens attack each other. Print a matrix satisfying the conditions with positions with
+queens marked with ‘1’ and empty spaces with ‘0’. You must solve the n queens problem
+using backtracking.
+'''
+
 def generate_board(N):
     # Generating a 2-D list with NxN elements
     # List comprehension  
@@ -14,8 +108,8 @@ def print_board(board):
     for row in board:
         print(i,end = " ") # printing vertical indices (just for reference)
         for item in row:
-            print(f"|{pieces[item]}|",end = "")  #Printing |chess_piece|
-            # print(f"|{item}|",end = "")
+            print(f"|{pieces[item]}|",end = "")  #Printing |chess_piece| (Qs and empty spaces) 
+            # print(f"|{item}|",end = "")        # This line prints it as 0s and 1s in matrix
         print()
         i += 1
     print()
@@ -87,3 +181,60 @@ board = generate_board(N)
 n_queen(board,0)
 print()
 print(f'Count = {count}')
+
+
+# Problem 3 
+'''We have a function F such that
+given two lists L1 and L2, f(L1, L2) returns the difference of the sum of the elements of L1
+and L2. Now given a list of size 3k that consists of 3k − 1 many 1s and a single 2, 
+find the position of 2 making at most k many calls to the function F. '''
+
+def F(L1,L2):
+    diff = 0
+    for i in L1 : diff += i
+    for j in L2 : diff -= j
+    return diff
+
+# Divide the list into three parts 
+def find_2(arr,left,right):
+    
+    # Converting list to 3k form (not needed for our case where n = 3^k)
+    if len(arr)%3 != 0:
+        for i in range(3-len(arr)%3):
+            arr.append(1)
+    
+    # Finding mid elements (Notion of distance)
+    mid1 = left + (right-left)//3
+    mid2 = mid1 + (right-left)//3
+    
+    if arr[left] == 2:
+        return left
+
+    elif arr[mid1] == 2:
+        return mid1
+    
+    elif arr[mid2] == 2:
+        return mid2
+
+    # Slicing lists into left , mid and right lists
+    left_list = arr[left:mid1]
+    mid_list = arr[mid1:mid2] # Not really needed but just for symmetry
+    right_list = arr[mid2:right]
+
+    # Calling difference function on left and right list
+    diff = F(left_list,right_list)
+
+    # If difference is 0 , 2 is in mid list
+    if diff  == 0:
+        return find_2(arr,mid1,mid2)
+
+    # If difference > 0 , 2 is in left list
+    elif diff  > 0:
+        return find_2(arr,left,mid1)
+    
+    # Else 2 in right list 
+    else:
+        return find_2(arr,mid2,right)
+
+list1 = [1,1,2,1,1,1,1,1,1]
+print(find_2(list1,0,len(list1)))
