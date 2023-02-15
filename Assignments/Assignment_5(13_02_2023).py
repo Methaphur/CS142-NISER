ea272e4642a9207c0447a62b04190d2187d6cabd
Harisankar <94229107+Methaphur@users.noreply.github.com>
2023-02-15 22:23:24 +0530

    Added Qs4 and updated Super queen

--- Assignments/Assignment_5(13_02_2023).py
+++ Assignments/Assignment_5(13_02_2023).py
@@ -33,12 +33,13 @@ def subset_sum(arr, target, length):
     
     return is_subset_sum(0, 0, [])
 
-
+print("Qs 1.a")
 input = [3,34,4,5,12,5,2]
 target = 9
 length = 2
+print(f'Input = {input} target = {target} length = {length}')
 print(subset_sum(input,target,length))
-
+print()
 # Problem 1.b)
 '''Given a set of positive and negative integers, and a value t, and an integer k determine
 if there is a subset of size exactly k, such that the sum of the elements in the set is equal to
@@ -79,17 +80,17 @@ def subset_sum(arr, target, length):
     
     return is_subset_sum(0, 0, [])
 
-
+print("Qs 1.b")
 input = [3,34,4,5,12,5,2,-3,-5,-4]
 target = -12
 length = 3
 print(subset_sum(input,target,length))
-
+print()
 # Problem 2
 # N Queen problem
-'''Given an n×n chess board, you must place n queens on the board so that no
+'''Given an nxn chess board, you must place n queens on the board so that no
 two queens attack each other. Print a matrix satisfying the conditions with positions with
-queens marked with ‘1’ and empty spaces with ‘0’. You must solve the n queens problem
+queens marked with '1' and empty spaces with '0'. You must solve the n queens problem
 using backtracking.
 '''
 
@@ -179,14 +180,104 @@ count = 0
 board = generate_board(N)    
 
 n_queen(board,0)
-print()
 print(f'Count = {count}')
+print()
+
+# Question 2.b)
+# Added a knight condition to N Queen problem and minor code change 
+# N Queen problem
+# I'm not sure if the logic is correct for this one , pls feel free to pull request
+def generate_board(N):
+    board = [[0 for i in range(N)] for i in range(N)]
+    return board
+
+def print_board(board):
+    pieces = {0:" ", 1:"Q" } 
+    print('   ',end= "")
+    print(*[i for i in range(len(board))],sep = "  ")
+    i = 0
+    for row in board:
+        print(i,end = " ")
+        for item in row:
+            print(f"|{pieces[item]}|",end = "")
+            # print(f"|{item}|",end = "")
+        print()
+        i += 1
+    print()
+
+def has_queen(row,column,board):
+    if board[row][column] == 1:
+        return True
+    else:
+        return False
+
+def valid_spot(row,column,board):
+
+    # Row condition
+    for i in range(len(board)):
+        if board[i][column] == 1:
+            return False
+
+    # Column condition
+    for col in range(len(board)):
+        if board[row][col] == 1:
+            return False
+     
+    # Right diagonal condition
+    rows , col = row,column
+    while rows >= 0 and col >= 0:
+        if board[rows][col] == 1:
+            return False
+        rows -= 1
+        col  -= 1
+
+    # Left diagonal condition
+    rows , col = row , column
+    while rows >= 0 and col < len(board):
+        if board[rows][col] == 1:
+            return False
+        rows -= 1
+        col += 1
+
+    # Knight condition 
+    possible_moves = [(1,2),(1,-2),(-1,2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
+    for (x,y) in possible_moves:
+        x = x + row
+        y = y + column
+
+        if x>= 0 and y>= 0 and x < len(board) and y < len(board):
+            if board[x][y] == 1:
+                return False
+
+    return True
+
+N = 8
+board = generate_board(N)
+def super_queen(board):
+    count = 0
+    max_queen = 0
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if not has_queen(row,col,board):
+                if valid_spot(row,col,board):
+                    board[row][col] = 1
+                    # print_board(board)
+                    count += 1
+        if max_queen < count:
+            max_queen = count
+    print_board(board)
+    return max_queen
+
+print("Qs 2.b)")
+print("Super Queen")
+print(f'Super Queen = {super_queen(board)}')
+
 
 
 # Problem 3 
 '''We have a function F such that
 given two lists L1 and L2, f(L1, L2) returns the difference of the sum of the elements of L1
-and L2. Now given a list of size 3k that consists of 3k − 1 many 1s and a single 2, 
+and L2. Now given a list of size 3k that consists of 3k - 1 many 1s and a single 2, 
 find the position of 2 making at most k many calls to the function F. '''
 
 def F(L1,L2):
@@ -236,5 +327,29 @@ def find_2(arr,left,right):
     else:
         return find_2(arr,mid2,right)
 
-list1 = [1,1,2,1,1,1,1,1,1]
+print("Qs 3.")
+list1 = [1,1,1,2,1,1,1,1,1]
+print(list1)
 print(find_2(list1,0,len(list1)))
+print()
+
+# Question 4 
+# Create a class in python
+print("Qs 4.")
+class Person:
+    def __init__(self,name,age):
+        self.name = name 
+        self.age = age 
+    
+    def greetings(self):
+        greeting = f'Hi! I am {self.name}'
+        return greeting
+    
+    def birthday(self):
+        self.age = self.age + 1
+    
+character_1 = Person("Methaphur",18)
+print(character_1.age)
+print(character_1.greetings())
+character_1.birthday()
+print(character_1.age)
